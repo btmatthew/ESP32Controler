581db80f31dbe2e73ab45f77d352612e9ed87b16
Matthew Bulat <matbulat@gmail.com>
2019-03-09 18:20:40 +0000

    Fixed the issue that uri didn't pick up prefix for url from settings option

--- app/src/main/java/com/matthewbulat/espiot/RetrofitDIR/ApiUtils.java
+++ app/src/main/java/com/matthewbulat/espiot/RetrofitDIR/ApiUtils.java
@@ -6,7 +6,10 @@ import com.matthewbulat.espiot.RetrofitDIR.Interfaces.IoTAPI;
 public class ApiUtils {
 
     public IoTAPI getIoTService() {
-        String BASE_URL = String.format("https://%s:%s",new PreferenceReader().readPreference("server_address"),new PreferenceReader().readPreference("port_number_web_server"));
+        String BASE_URL = String.format("%s%s:%s",
+                new PreferenceReader().readPreference("api_address_prefix"),
+                new PreferenceReader().readPreference("server_address"),
+                new PreferenceReader().readPreference("port_number_web_server"));
         return new APIClient().getClient(BASE_URL).create(IoTAPI.class);
     }
 }
